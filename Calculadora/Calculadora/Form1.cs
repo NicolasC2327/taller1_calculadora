7225caaf0acdff14011d82f31c3792f42877d9ce
unknown <admin@BGAI203-21.bga.upb>
2023-08-18 19:11:36 -0500

    proyecto inicial/ Diseño calculadora

---
+++ Calculadora/Calculadora/Form1.cs
@@ -0,0 +1,30 @@
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtResultado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
